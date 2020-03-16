f5945daadd61a8a85452660260c60a4094efeb54
ASUS <ASUS@LAPTOP-1QOLQD43>
2020-03-16 21:48:27 +0300

    First Commit To ......

---
+++ src/helloWorld.java
@@ -0,0 +1,11 @@
+public class helloWorld {
+	public static void main(String[] args) {
+		String result = hellowWorld();
+
+		System.out.println(result);
+	}
+
+	private static String hellowWorld() {
+		return "Hellow World";
+	}
+}
